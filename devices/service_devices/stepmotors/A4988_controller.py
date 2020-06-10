098f75a1be3533416c048a4bce470a66fe4b55b6
saldenisov <sergey.denisov@u-psud.fr>
2020-06-10 11:31:25 +0200

    A4988 update

--- devices/service_devices/stepmotors/A4988_controller.py
+++ devices/service_devices/stepmotors/A4988_controller.py
@@ -34,12 +34,6 @@ class StpMtrCtrl_a4988_4axes(StpMtrController):
         self._ttl = None  # to make controller work when dev_mode is ON
         super().__init__(**kwargs)
 
-    def activate(self, func_input: FuncActivateInput) -> FuncActivateOutput:
-        res, comments = super().activate(func_input)
-        if res:
-            self._disable_controller()
-        return res, comments
-
     def _connect(self, flag: bool) -> Tuple[bool, str]:
         res, comments = super()._connect(flag)
         if res:
