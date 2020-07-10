bd547998ffb6c614ebc05bc320b8382255a7ee17
saldenisov <sergey.denisov@u-psud.fr>
2020-07-10 16:53:59 +0200

    BaslerCtrl started

--- devices/service_devices/cameras/Basler_cameras.py
+++ devices/service_devices/cameras/Basler_cameras.py
@@ -6,5 +6,10 @@ created
 from devices.service_devices.cameras.camera_controller import CameraCtrl
 
 
-class BaslerGiGE(CameraCtrl):
-    pass
+class CameraCtrl_Basler(CameraCtrl):
+    """
+    Works only for GiGE cameras of Basler, usb option is not available
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
