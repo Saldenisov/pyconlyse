0b9833539793a42612a42a319d27a592177a7d95
saldenisov <sergey.denisov@u-psud.fr>
2022-01-13 15:52:26 +0100

    tango-start updated

--- bin/start_all_LaserPointing.cmd
+++ bin/start_all_LaserPointing.cmd
@@ -1,11 +1,8 @@
 @echo off
 call %ANACONDA%/Scripts/activate.bat %ANACONDA%
 ECHO 'Starting NETIO axes'
-set list=1_Cam1 2_Cam2
+set list=1_Cam1 2_Cam2 3_Cam3
 timeout 1
 for %%x in (%list%) do (
 start /min cmd /k "cd %PYCONLYSE%\DeviceServers\LaserPointing & conda activate %PYCONLYSE_ENV% & python DS_LaserPointing.py %%x"
 )
-
-
-
