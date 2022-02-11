4baa5d6a0720be341dfbac546990995a3a0d4a68
saldenisov <sergey.denisov@u-psud.fr>
2022-02-11 15:21:20 +0100

    dll updated 64 bit added, code of DS_ANDOR_CCD.py updated

--- DeviceServers/ANDOR_CCD/add_ds_ANDOR_CCD.py
+++ DeviceServers/ANDOR_CCD/add_ds_ANDOR_CCD.py
@@ -3,11 +3,12 @@ from tango import DbDevInfo, Database
 db = Database()
 
 names = {7428: ['manip/CR', 'ANDOR_CCD_V0', 'ANDOR_CCD1', 7428,
-                {'Acquisition_Controls': {'SetAcquisitionMode': 3, 'SetTriggerMode': 1, 'SetFastExtTrigger': False,
-                                              'SetReadMode': 1, 'SetMultiTrack': (2, 128, 0), 'SetExposureTime': 0.0001,
-                                              'SetHSSpeed': 1, 'SetVSSpeed': 1, 'SetADChannel': 1, 'SetPreAmpGain': 0,
-                                              'SetBaselineClamp': True, 'SetTemperature': -80, 'CoolerON': True
-                                              }}
+                {'Acquisition_Controls': {'AcquisitionMode': 3, 'TriggerMode': 1, 'FastExtTrigger': False,
+                                          'ReadMode': 1, 'MultiTrack': (2, 128, 0), 'ExposureTime': 0.0001,
+                                          'HSSpeed': 1, 'VSSpeed': 1, 'ADChannel': 1, 'PreAmpGain': 0,
+                                          'BaselineClamp': True, 'Temperature': -80, 'CoolerON': True
+                                              }},
+                 {'width': 1024}, 'C:/dev/pyconlyse/DeviceServers/ANDOR_CCD/atmcd32d.dll'
                 ]
          }
 
@@ -23,7 +24,8 @@ def main():
         dev_info.server = f'DS_ANDOR_CCD/{i}_{val[2]}'
         db.add_device(dev_info)
         db.put_device_property(dev_name, {'device_id': dev_id, 'friendly_name': val[1], 'server_id': i,
-                                          'serial_number': val[3], 'parameters': str(val[4])})
+                                          'serial_number': val[3], 'parameters': str(val[4]), 'width': val[5]['width'],
+                                          'dll_path': val[6]})
         i += 1
 
 
