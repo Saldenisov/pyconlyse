189de9dcb34de81e155ff44afaeba188ecfbd4b7
dobrovolskii <dsdobrovolskii@gmail.com>
2022-02-18 15:52:29 +0100

    translation_stage_fix_number1

--- utilities/mytests/Denis test1.py
+++ utilities/mytests/Denis test1.py
@@ -5,10 +5,10 @@ param = {'MS_for_A4988': {'friendly_name': 'MS_for_A4988', 'pins': {1: 1, 2: 1, 
             '4_A4988': {'friendly_name': 'xuy', 'pins': {7: 0}},
             '5_A4988': {'friendly_name': 'xuy', 'pins': {8: 0}},
             '1_DM542': {'friendly_name': 'xuy', 'pins': {9: 0}},
-            '2_DM542': {'friendly_name': 'xuy', 'pins': {10: 0}},
-            '3_DM542': {'friendly_name': 'xuy', 'pins': {21: 0}},
+            '2_DM542': {'friendly_name': 'xuy', 'pins': {'A': 0}},
+            '3_DM542': {'friendly_name': 'xuy', 'pins': {'B': 0}},
          }
-
+import time
 # connect to numato
 import telnetlib
 from time import sleep
@@ -45,7 +45,7 @@ def form_cmd(pin_n, value):
     return cmd.encode('ascii') + b'\r\n'
 
 def send_to_numato(cmd):
-    print(cmd)
+    # print(cmd)
     tn.write(cmd)
 
 def set_pin(pin_n, value):
@@ -59,3 +59,25 @@ def set_pin(pin_n, value):
 for device_name, parameters in param.items():
     for pin_n, value in parameters['pins'].items():
         set_pin(pin_n, value)
+
+a = time.time()
+for i in range(3000):
+    # sleep(0.1)
+    if i%2 == 0:
+        value = 0
+    else:
+        value = 1
+
+    send_to_numato(form_cmd(1, value))
+tn.read_eager()
+for i in range(2100):
+    # sleep(0.1)
+    if i%2 == 0:
+        value = 0
+    else:
+        value = 1
+
+    send_to_numato(form_cmd(1, value))
+
+b = time.time()
+print(f'Time elapsed: {b - a}')
