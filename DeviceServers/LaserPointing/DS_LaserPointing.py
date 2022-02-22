a04ffc6c095424919c87427c96ede56841dac2cb
saldenisov <sergey.denisov@u-psud.fr>
2022-02-22 15:59:31 +0100

    optimization bounds update of each cycle

--- DeviceServers/LaserPointing/DS_LaserPointing.py
+++ DeviceServers/LaserPointing/DS_LaserPointing.py
@@ -20,6 +20,8 @@ try:
 except ModuleNotFoundError:
     from General.DS_Control import DS_ControlPostion
 
+global global_result
+global glob_res_n
 
 class DS_LaserPointing(DS_ControlPosition):
     RULES = {'set_param_after_init': [DevState.ON], 'start_grabbing': [DevState.ON],
@@ -46,11 +48,11 @@ class DS_LaserPointing(DS_ControlPosition):
         camera_name = self.ds_dict['Camera']
         self.devices[camera_name] = Device(camera_name)
         self.turn_on()
-        # self.locking_client_token = 'test'
-        # self.locked_client = True
-        # # self.start_cgc('test')
+        self.locking_client_token = 'test'
+        self.locked_client = True
+        self.start_cgc('test')
 
-    def set_optimization_thread(self, opt_name: str, actuator_name: str, group_points: str):
+    def set_optimization_thread(self, opt_name: str, actuator_name: str, group_points: str, bounds):
         actuators = self.groups[actuator_name]
         actuators_devices = od()
         for actuator in actuators:
@@ -60,6 +62,7 @@ class DS_LaserPointing(DS_ControlPosition):
 
         optimization_group = Thread(target=self.minimization, args=[list(actuators_devices.values()),
                                                                     self.pid_groups[group_points], opt_name])
+        setattr(self, f'{opt_name}_bounds', bounds)
         self.optimization_names.append(opt_name)
         optimization_group.start()
 
@@ -84,8 +87,8 @@ class DS_LaserPointing(DS_ControlPosition):
         # if client_token == self.locking_client_token and self.locked_client:
         if True:
             self.cgc = True
-            self.set_optimization_thread('opt1', 'Actuators 1', 'group1')
-            self.set_optimization_thread('opt2', 'Actuators 2', 'group2')
+            self.set_optimization_thread('opt1', 'Actuators 1', 'group1', [(-40, 40), (-40, 40)])
+            self.set_optimization_thread('opt2', 'Actuators 2', 'group2', [(-40, 40), (-40, 40)])
             self.active_opt = self.optimization_names[0]
             return 0
         else:
@@ -104,12 +107,14 @@ class DS_LaserPointing(DS_ControlPosition):
 
     def minimization(self, controlling_devices: List[Device], points_group: str, opt_name: str):
         self.controlling_cycle = 0
+        self.glob_res_n = 0
+        self.global_result = 0
 
         def min_func(x):
             print(f'Trying {opt_name} with {x}')
             if self.active_opt == opt_name:
                 for device, pos in zip(controlling_devices, x):
-                    self.execute_action(pos, device, True)
+                    self.execute_action(pos, device)
 
                 for point in points_group:
                     point_rules = self.controller_rules[point]
@@ -126,33 +131,65 @@ class DS_LaserPointing(DS_ControlPosition):
                     self.previos_pos = self.current_pos
                     self.current_pos = cg
 
-                delta_x = self.previos_pos['X'] - self.current_pos['X']
-                delta_y = self.previos_pos['Y'] - self.current_pos['Y']
+                if self.previos_pos['X'] == 1024 and self.current_pos['X'] == 1024:
+                    delta_x = 1024
+                else:
+                    delta_x = self.previos_pos['X'] - self.current_pos['X']
+
+                if self.previos_pos['Y'] == 1024 and self.current_pos['Y'] == 1024:
+                    delta_y = 1024
+                else:
+                    delta_y = self.previos_pos['Y'] - self.current_pos['Y']
+
                 self.deltas = [delta_x, delta_y]
 
-                if abs(delta_x) <= 0.5 and abs(delta_y) <= .5:
+                if (abs(delta_x) <= 2 and abs(delta_y) <= 2) or self.glob_res_n >= 5:
                     active_opt_idx = self.optimization_names.index(self.active_opt)
                     new_active_opt_idx = 0
                     if active_opt_idx == 0:
                         new_active_opt_idx = 1
                     self.active_opt = self.optimization_names[new_active_opt_idx]
                     res = 0
+                    self.glob_res_n = 0
+                    x_pos, y_pos = x
+                    bounds = getattr(self, f'{opt_name}_bounds')
+
+                    x_min = x_pos - abs(bounds[0][0] / 1.5)
+                    x_max = x_pos + abs(bounds[0][1] / 1.5)
+                    y_min = y_pos - abs(bounds[1][0] / 1.5)
+                    y_max = y_pos + abs(bounds[1][1] / 1.5)
+                    setattr(self, f'{opt_name}_bounds', [(x_min, x_max), (y_min, y_max)])
                 else:
                     res = (delta_x**2 + delta_y**2)**.5
             else:
                 res = 0
             print(f'Deltas: {self.deltas}...Res: {res}')
 
+            if res != 0:
+                ratio = abs(self.global_result) / abs(res)
+            else:
+                ratio = 1
+
+            if (ratio >= 0.9 and ratio <= 1.1) and res < 1448:
+                self.glob_res_n += 1
+                print(f'Glob res_n: {self.glob_res_n}')
+            else:
+                self.glob_res_n = 0
+
+            self.global_result = res
+
             return res
 
 
         while self.cgc:
             if self.active_opt == opt_name:
-                # minimize(fun=min_func, x0=[0, 0], args=([controlling_devices, self, opt_name]), )
+                bounds = getattr(self, f'{opt_name}_bounds')
+                print(f'{opt_name} bounds: {bounds} ')
+                minimize(fun=min_func, x0=[0, 0], bounds=bounds)
                 # basinhopping(func=min_func, x0=[0, 0], niter=100, T=1.0, stepsize=5, interval=2,
                 #              disp=True, target_accept_rate=0.5, stepwise_factor=0.9)
-                dual_annealing(func=min_func, x0=[0, 0], maxiter=200, no_local_search=True,
-                               bounds=[(-150, 150), (-150, 150)])
+                # dual_annealing(func=min_func, x0=[0, 0], maxiter=200, no_local_search=True,
+                #                bounds=bounds, maxfun=10)
 
                 # minimize(fun=min_func, x0=[0, 0], args=([controlling_devices, self, opt_name]), method='TNC',
                 #          bounds=[(-100, 100)] * 2, jac='2-point',
