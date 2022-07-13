a3a1ce281591584e81f9ad63b582095b9b8b8359
saldenisov <saldenisov@gmail.com>
2022-07-13 09:25:04 +0200

    gpio test

--- utilities/mytests/RPI3_GPIO.py
+++ utilities/mytests/RPI3_GPIO.py
@@ -4,9 +4,9 @@ from gpiozero import LED
 
 factory = PiGPIOFactory(host='10.20.30.205')
 
-pin1 = LED(pin='1', pin_factory=factory)
-
-for i in range(1000):
-    print(f'{i}')
-    pin1.on()
-    pin1.off()
+pin1 = LED(pin='4', pin_factory=factory)
+pin1.off()
+# for i in range(1000):
+#     print(f'{i}')
+#     pin1.on()
+#     pin1.off()
