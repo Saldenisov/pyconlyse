e824ddc427d60f85a267ca526f705912aa082529
saldenisov <sergey.denisov@universite-paris-saclay.fr>
2025-09-26 09:04:00 +0200

    final commit

--- bin/start_tango_elysium.cmd
+++ bin/start_tango_elysium.cmd
@@ -1,9 +1,7 @@
 start cmd /c %PYCONLYSE%\bin\start_all_STANDA.cmd
-start cmd /c %PYCONLYSE%\bin\start_all_TOPDIRECT.cmd
-start cmd /c %PYCONLYSE%\bin\start_all_NETIO.cmd
 timeout 20
 start /min cmd /c %TANGO_ROOT%\bin\start-astor.bat
 timeout 1
 start /min cmd /c %TANGO_ROOT%\bin\start-jive.bat
 timeout 60
-start cmd /c %TANGO_ROOT%\bin\Starter.exe elysium
+start cmd /c %TANGO_ROOT%\bin\Starter.exe elysium2
